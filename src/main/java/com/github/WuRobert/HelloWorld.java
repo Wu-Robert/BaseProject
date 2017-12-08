53eec97fad058175055ae112367bc3c3c3f9977c
PC234 <sos7530631@gmail.com>
2017-12-08 20:01:43 +0800

    fail

---
+++ src/main/java/com/github/WuRobert/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.github.WuRobert;
+
+public class HelloWorld {
+
+    public String fcu(int a){
+        if(a>5)return "是";
+        else return "不是";
+    }
+}
---
+++ src/test/java/com/github/WuRobert/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.github.WuRobert;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void fcu() throws Exception {
+        String o="是";
+
+    }
+
+}
